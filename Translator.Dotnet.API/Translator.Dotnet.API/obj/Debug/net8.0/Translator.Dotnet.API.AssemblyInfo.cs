61b5c32555243f24d92c32cde2e9693fecf90a8d
Rayne <raynechiang5235@gmail.com>
2025-04-19 00:45:05 +0200

    🐞 fix: fix config

--- Translator.Dotnet.API/Translator.Dotnet.API/obj/Debug/net8.0/Translator.Dotnet.API.AssemblyInfo.cs
+++ Translator.Dotnet.API/Translator.Dotnet.API/obj/Debug/net8.0/Translator.Dotnet.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Translator.Dotnet.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2b3496efaf71916be5d30866d6369e0c94bcad6e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6dc06f5a6c564f4513fa4c07149949bd7a567a22")]
 [assembly: System.Reflection.AssemblyProductAttribute("Translator.Dotnet.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Translator.Dotnet.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
